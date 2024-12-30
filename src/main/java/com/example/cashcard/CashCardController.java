63d427c6c7ae49eb9660c0ed959634568d6ee7ee
Vamshi Rathnaa Prabodh <vamshirathnaa@prabodh.in>
2024-12-30 16:04:28 +0530

    Added CashCardRepository interface

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -6,9 +6,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/cashcards")
 class CashCardController {
-    private ResponseEntity<String> findById() {
-        return ResponseEntity.ok("{}");
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
-
 }
